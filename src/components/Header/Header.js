de7eca772e4096b5cc1157e687da8641bccac6d9
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-09 01:59:44 +0600

    Implement Auth Redirect and added vip route for only logged in user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,6 +16,11 @@ const Header = () => {
                 <Link to="/products">Products</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/register">Register</Link>
+                {
+                    user && <>
+                        <Link to="/vip">VIP</Link>
+                    </>
+                }
                 <span>{user?.displayName && user.displayName}</span>
                 {
                     user?.uid ? <button onClick={() => signOut(auth)}>Sign out</button> :
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,24 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
-import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './Login.css';
 
 const auth = getAuth();
 
 const Login = () => {
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const location = useLocation()
+    const navigate = useNavigate();
+
+    const from = location?.state?.from?.pathname || '/'
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
     return (
         <div>
             <h2>Please login here !</h2>
@@ -17,8 +29,8 @@ const Login = () => {
                 <br />
                 <input type="submit" value="Login" />
             </form>
-            <div style={{margin:'20px'}}>
-                <button onClick={() => signInWithGoogle()}>Google Sign In</button>
+            <div style={{ margin: '20px' }}>
+                <button onClick={handleGoogleSignIn}>Google Sign In</button>
             </div>
         </div>
     );

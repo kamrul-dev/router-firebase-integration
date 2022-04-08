071b59d073ef2c0766605396666664f45dd6a8c6
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-09 01:13:09 +0600

    Protected Route and Require Auth

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Orders.css';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>This is Order page</h2>
+        </div>
+    );
+};
+
+export default Orders;
